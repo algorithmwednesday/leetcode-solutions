1337e4496649ae6edc1d38e6c3c4b55314b72af8
theterminalguy <damiansimonpeter@gmail.com>
2022-10-17 11:22:54 -0400

    Add readme

--- 217/go/main.go
+++ 217/go/main.go
@@ -1,6 +1,7 @@
 // package main
 // 
-// this is a solution to LeetCode problem 217 
+// This is a solution to LeetCode problem 217. You can find the accompanying video tutorial here:
+// 	- https://youtu.be/W2F8IIlo_-4?list=PLgqHOPTffQL5TYNTswlhk0hElEykSMIbb
 
 package main
 
